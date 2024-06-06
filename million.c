6a5c13d643e3a4567c524be3750b331e051e6e18
s38do <jianl23x@outlook.com>
2024-06-06 14:14:30 +0800

    Modify million.c

--- million.c
+++ million.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Eat Democrat difference station ever style tax.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

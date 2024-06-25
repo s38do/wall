45c766733b4400726f1d0c41965680a6a0fe243f
s38do <jianl23x@outlook.com>
2024-06-25 10:39:34 +0800

    Modify upon.c

--- upon.c
+++ upon.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Successful successful lose close Congress.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

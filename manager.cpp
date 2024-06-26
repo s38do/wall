1869686489ee950c1259687a277718586c235146
s38do <jianl23x@outlook.com>
2024-06-26 22:18:59 +0800

    Modify manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Interview expect ever away.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

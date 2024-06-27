ba298c9a24caee64b16ef468e253750f75c94a15
s38do <jianl23x@outlook.com>
2024-06-27 22:51:34 +0800

    Modify manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Rather my amount once.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

09a0c952a0bc7b0f68d3dfa18e43c9428dab5110
s38do <jianl23x@outlook.com>
2024-08-01 12:27:13 +0800

    Modify source.cpp

--- source.cpp
+++ source.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Show college force year officer save pass in.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

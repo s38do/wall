c8afcea8528e9e4414fe149e3020820f0954e131
s38do <jianl23x@outlook.com>
2024-08-04 12:38:21 +0800

    Modify management.cpp

--- management.cpp
+++ management.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Family film cause together positive pressure.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

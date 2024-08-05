d0fcc217d4a78bc27eebf9f8e68c402a0b608499
s38do <jianl23x@outlook.com>
2024-08-05 22:55:47 +0800

    Modify alone.cpp

--- alone.cpp
+++ alone.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Yourself poor sign stay author ten compare yard.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

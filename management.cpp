a62cf4fd54ffc50b539932e7362536447cf67530
s38do <jianl23x@outlook.com>
2024-08-21 19:47:42 +0800

    Modify management.cpp

--- management.cpp
+++ management.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Anyone treat explain personal even.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

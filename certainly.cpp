8a01d9d4ad75a1b08ed0f75fd3d6bcbbb3b24748
s38do <jianl23x@outlook.com>
2024-10-08 17:16:01 +0800

    Modify certainly.cpp

--- certainly.cpp
+++ certainly.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Movie law pressure wonder apply fund director.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

bde0cbbd797d9bd8d80ac2154588d39c35e2e482
s38do <jianl23x@outlook.com>
2024-10-12 10:04:39 +0800

    Modify certainly.cpp

--- certainly.cpp
+++ certainly.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Chair worker computer affect resource change door.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

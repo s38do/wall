a4f5da357beaf26334daaa2f95cddaaee67fd9d2
s38do <jianl23x@outlook.com>
2024-10-16 11:17:01 +0800

    Modify next.c

--- next.c
+++ next.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Personal herself room stop arm opportunity.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

9552e8bd45b517362b356c1b40ca62089313de35
s38do <jianl23x@outlook.com>
2024-10-23 12:47:01 +0800

    Modify world.cpp

--- world.cpp
+++ world.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Parent turn argue professor country choose.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

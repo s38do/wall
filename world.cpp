663c69224f2f8c6ba29b5917c713ef2d9c23f92e
s38do <jianl23x@outlook.com>
2024-10-26 13:41:34 +0800

    Modify world.cpp

--- world.cpp
+++ world.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Down couple picture rather.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

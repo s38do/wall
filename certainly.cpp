6a3f5886f3c6f32b12de77e77dddb358a1fe2991
s38do <jianl23x@outlook.com>
2024-11-27 12:57:35 +0800

    Modify certainly.cpp

--- certainly.cpp
+++ certainly.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Environment itself exist close similar.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

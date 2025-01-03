260b4c62aaeb289c4464173d5184c0afa1d7ecf3
s38do <jianl23x@outlook.com>
2025-01-03 16:39:48 +0800

    Modify which.go

--- which.go
+++ which.go
@@ -17,3 +17,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Compare including land mission option.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

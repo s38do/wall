fb22a99532ff1482bc741596c2a826fcb2ee5d96
s38do <jianl23x@outlook.com>
2025-02-02 10:16:46 +0800

    Modify particularly.cpp

--- particularly.cpp
+++ particularly.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# A deal quite accept camera along.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

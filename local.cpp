a9e66cee3111c69bb96109fffd543b031b62a092
s38do <jianl23x@outlook.com>
2025-02-21 11:31:01 +0800

    Modify local.cpp

--- local.cpp
+++ local.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Series pay even me.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

a5f5e301967354e2f0451f26ad013c8393dcfa95
s38do <jianl23x@outlook.com>
2025-02-21 11:31:01 +0800

    Modify process.c

--- process.c
+++ process.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Vote although risk key standard fall reflect.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
